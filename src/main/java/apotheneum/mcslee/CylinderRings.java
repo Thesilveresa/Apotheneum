97b09533586b2667ff56317d27e33e41e5ff96eb
Mark Slee <mcslee@gmail.com>
2025-04-14 16:01:17 +0100

    Cosmogenesis content

---
+++ src/main/java/apotheneum/mcslee/CylinderRings.java
@@ -0,0 +1,47 @@
+package apotheneum.mcslee;
+
+import apotheneum.Apotheneum;
+import apotheneum.ApotheneumPattern;
+import heronarts.lx.LX;
+import heronarts.lx.LXCategory;
+import heronarts.lx.LXComponentName;
+import heronarts.lx.color.LXColor;
+import heronarts.lx.model.LXPoint;
+import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.utils.LXUtils;
+
+@LXCategory("Apotheneum/mcslee")
+@LXComponentName("Cylinder Rings")
+public class CylinderRings extends ApotheneumPattern {
+
+  public final CompoundParameter pos =
+    new CompoundParameter("Pos", 0, 360)
+    .setUnits(CompoundParameter.Units.DEGREES)
+    .setDescription("Base ring position");
+
+  public final CompoundParameter skew =
+    new CompoundParameter("Skew", 0)
+    .setUnits(CompoundParameter.Units.PERCENT_NORMALIZED)
+    .setDescription("Skew of the ring position");
+
+  public CylinderRings(LX lx) {
+    super(lx);
+    addParameter("pos", this.pos);
+    addParameter("skew", this.skew);
+  }
+
+  @Override
+  protected void render(double deltaMs) {
+    for (Apotheneum.Cylinder.Ring ring : Apotheneum.cylinder.exterior.rings) {
+      float angle = (this.pos.getValuef() + ring.index * skew.getValuef()) % 360f;
+      float pa = 0;
+      for (LXPoint p : ring.points) {
+        float dist = LXUtils.wrapdistf(angle, pa, 360f);
+        colors[p.index] = LXColor.gray(LXUtils.max(0, 100 - dist));
+        pa += 3;
+      }
+    }
+    copyCylinderExterior();
+  }
+
+}
