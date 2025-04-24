840d4f9eef78a9ddd6f2f708767df4c49c4d8ac0
Mark Slee <mcslee@gmail.com>
2025-04-24 09:41:39 +0100

    Add copyMirror utility for cube faces

--- src/main/java/apotheneum/ApotheneumPattern.java
+++ src/main/java/apotheneum/ApotheneumPattern.java
@@ -2,6 +2,7 @@ package apotheneum;
 
 import heronarts.lx.LX;
 import heronarts.lx.color.LXColor;
+import heronarts.lx.model.LXModel;
 import heronarts.lx.pattern.LXPattern;
 
 public abstract class ApotheneumPattern extends LXPattern {
@@ -39,24 +40,36 @@ public abstract class ApotheneumPattern extends LXPattern {
     copyCylinderExterior();
   }
 
+  protected void copyMirror(Apotheneum.Cube.Face from, Apotheneum.Cube.Face to) {
+    assertExists();
+    if ((from != null) && (to != null)) {
+      int colIndex = 0;
+      for (LXModel fromCol : from.columns) {
+        LXModel toCol = to.columns[to.columns.length - 1 - colIndex];
+        System.arraycopy(colors, fromCol.points[0].index, colors, toCol.points[0].index, fromCol.size);
+        ++colIndex;
+      }
+    }
+  }
+
   protected void copy(Apotheneum.Cube.Face from, Apotheneum.Cube.Face to) {
     assertExists();
     if ((from != null) && (to != null)) {
-      System.arraycopy(this.colors, from.model.points[0].index, colors, to.model.points[0].index, from.model.size);
+      System.arraycopy(colors, from.model.points[0].index, colors, to.model.points[0].index, from.model.size);
     }
   }
 
   protected void copy(Apotheneum.Cube.Orientation from, Apotheneum.Cube.Orientation to) {
     assertExists();
     if ((from != null) && (to != null)) {
-      System.arraycopy(this.colors, from.front.model.points[0].index, colors, to.front.model.points[0].index, from.size);
+      System.arraycopy(colors, from.front.model.points[0].index, colors, to.front.model.points[0].index, from.size);
     }
   }
 
   protected void copy(Apotheneum.Cylinder.Orientation from, Apotheneum.Cylinder.Orientation to) {
     assertExists();
     if ((from != null) && (to != null)) {
-      System.arraycopy(this.colors, from.columns[0].points[0].index, colors, to.columns[0].points[0].index, from.size);
+      System.arraycopy(colors, from.columns[0].points[0].index, colors, to.columns[0].points[0].index, from.size);
     }
   }
 
