f730c4bfcbcb488a325d5540e2e07e6a5e0ebbba
Thesilveresa <theresa.silver@gmail.com>
2025-06-19 09:14:34 -0400

    Update FirstAxis.java
    Corrected use of addParameter

--- src/main/java/apotheneum/thesilveresa/FirstAxis.java
+++ src/main/java/apotheneum/thesilveresa/FirstAxis.java
@@ -26,9 +26,9 @@ public class FirstAxis extends ApotheneumPattern {
 
   public FirstAxis(LX lx) {
     super(lx);
-    addParameter(symmetry);
-    addParameter(lineWidth);
-    addParameter(brightness);
+    addParameter("symmetry", this.symmetry);
+    addParameter("lineWidth", this.lineWidth);
+    addParameter("brightness", this.brightness);
   }
 
   @Override
