3765030ed27f9ed86aa0e9a54b1f7f632dca636b
Mark Slee <mcslee@gmail.com>
2025-07-21 15:37:39 +0100

    Add project opening to Apotheneum launcher

--- src/main/java/apotheneum/ApotheneumLauncherPlugin.java
+++ src/main/java/apotheneum/ApotheneumLauncherPlugin.java
@@ -18,25 +18,50 @@
 
 package apotheneum;
 
+import java.io.File;
 import java.util.Scanner;
 
 import heronarts.lx.LX;
 import heronarts.lx.LXPlugin;
+import heronarts.lx.osc.LXOscEngine;
 import heronarts.lx.osc.LXOscListener;
 import heronarts.lx.osc.OscMessage;
 
 @LXPlugin.Name("Apotheneum Launcher")
 public class ApotheneumLauncherPlugin implements LXPlugin, LXOscListener {
 
+  private LX lx;
+
   @Override
   public void initialize(LX lx) {
+    this.lx = lx;
     lx.engine.osc.addListener(this);
   }
 
   @Override
   public void oscMessage(OscMessage message) {
     try {
-      if (message.matches("/apotheneum/openLiveProject")) {
+      if (message.matches("/apotheneum/openProject")) {
+        final String fileName = message.getString();
+        final File projectFile = this.lx.getMediaFile(LX.Media.PROJECTS, fileName);
+        if (!projectFile.exists()) {
+          error("Requested non-existent project file: " + projectFile);
+          return;
+        }
+        if (projectFile.equals(this.lx.getProject())) {
+          log("Requested project file is already open, ignoring: " + projectFile);
+          return;
+        }
+        // Schedule project to be loaded on next loop pass
+        lx.engine.addTask(() -> {
+          try {
+            log("Opening project file: " + projectFile);
+            this.lx.openProject(projectFile);
+          } catch (Throwable x) {
+            LXOscEngine.error(x, "Error opening project \"" + projectFile + "\": " + x.getMessage());
+          }
+        });
+      } else if (message.matches("/apotheneum/openLiveProject")) {
         final String home = System.getProperty("user.home");
         final String liveProject = home + "/" + message.getString();
         new Thread(() -> {
@@ -45,10 +70,10 @@ public class ApotheneumLauncherPlugin implements LXPlugin, LXOscListener {
             final Process p = Runtime.getRuntime().exec(new String[] {
               "osascript", home + "/Desktop/OpenLiveProject.scpt", liveProject }
             );
-            try (Scanner scanner = new Scanner(p.getInputStream())) {
+            try (Scanner scanner = new Scanner(p.getInputStream()).useDelimiter("\n")) {
               scanner.forEachRemaining(ApotheneumLauncherPlugin::log);
             }
-            try (Scanner scanner = new Scanner(p.getErrorStream())) {
+            try (Scanner scanner = new Scanner(p.getErrorStream()).useDelimiter("\n")) {
               scanner.forEachRemaining(ApotheneumLauncherPlugin::error);
             }
           } catch (Exception x) {
